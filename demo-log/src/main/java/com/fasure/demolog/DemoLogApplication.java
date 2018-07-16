14de1490cb35d1b06c20a7cb92972e120a9c04d2
fasure <fasurhtf@163.com>
2018-07-16 08:44:47 +0800

    init

---
+++ demo-log/src/main/java/com/fasure/demolog/DemoLogApplication.java
@@ -0,0 +1,12 @@
+package com.fasure.demolog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoLogApplication.class, args);
+    }
+}
---
+++ demo-log/src/main/java/com/fasure/demolog/controller/LogController.java
@@ -0,0 +1,27 @@
+package com.fasure.demolog.controller;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/log")
+public class LogController {
+    private static final Logger logger = LoggerFactory.getLogger(LogController.class);
+
+    @RequestMapping("/demo")
+    public String test() {
+
+        logger.debug("debug log");
+
+        logger.info("info log");
+
+        logger.warn("warn log");
+
+        logger.error("error log");
+
+        return "hello world";
+    }
+}
