0dbd1e3a43ea42fd3de14b6ec0add34ba16fad02
fasure <fasurhtf@163.com>
2018-07-17 08:31:34 +0800

    add demo-dubbo init , begin dubbo-mybatis

---
+++ demo-dubbo/src/main/java/com/fasure/demodubbo/DemoDubboApplication.java
@@ -0,0 +1,12 @@
+package com.fasure.demodubbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDubboApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoDubboApplication.class, args);
+    }
+}
--- demo-mybatis/src/main/java/com/fasure/demomybatis/DemoMybatisApplication.java
+++ demo-mybatis/src/main/java/com/fasure/demomybatis/DemoMybatisApplication.java
@@ -1,8 +1,11 @@
 package com.fasure.demomybatis;
 
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//@EnableAutoConfiguration(exclude={DruidDataSourceAutoConfigure.class})
 @SpringBootApplication
 public class DemoMybatisApplication {
 
